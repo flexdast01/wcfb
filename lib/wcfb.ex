d5a5cef5da258bfeb2442ace339dd023ad4f301c
Daniel Strömberg <daniel.stromberg@flexapplications.se>
2024-11-22 11:42:18 +0100

    Initial commit

---
+++ lib/wcfb.ex
@@ -0,0 +1,18 @@
+defmodule Wcfb do
+  @moduledoc """
+  Documentation for `Wcfb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wcfb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/wcfb_test.exs
@@ -0,0 +1,8 @@
+defmodule WcfbTest do
+  use ExUnit.Case
+  doctest Wcfb
+
+  test "greets the world" do
+    assert Wcfb.hello() == :world
+  end
+end
